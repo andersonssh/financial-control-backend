ef3de2f2181d55deceab796382f12dbd0b348122
Anderson Araujo <andersondev.ssh@gmail.com>
2023-10-19 23:17:34 -0300

    Improve database module

--- app/database.py
+++ app/database.py
@@ -1,23 +1,42 @@
 import os
+import copy
 from typing import List
 
 from pymongo import MongoClient
 
-MONGO_USER = os.environ["MONGO_USER"]
-MONGO_PASSWORD = os.environ["MONGO_PASSWORD"]
-MONGO_HOST = os.environ["MONGO_HOST"]
-MONGO_DATABASE = os.environ["MONGO_DATABASE"]
-MONGO_URL = f"{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}/{MONGO_DATABASE}"
-MONGO_OPTIONS = "retryWrites=true&w=majority"
+mongo_host = os.environ["MONGO_HOST"]
+mongo_database = os.environ["MONGO_DATABASE"]
+if mongo_host == "localhost":
+    client = MongoClient()
+else:
+    mongo_user = os.environ["MONGO_USER"]
+    mongo_password = os.environ["MONGO_PASSWORD"]
+    mongo_url = f"{mongo_user}:{mongo_password}@{mongo_host}/{mongo_database}"
+    mongo_options = "retryWrites=true&w=majority"
+    client = MongoClient(f"mongodb+srv://{mongo_url}?{mongo_options}")
 
-client = MongoClient(f"mongodb+srv://{MONGO_URL}?{MONGO_OPTIONS}")
-
-db = client.get_database(MONGO_DATABASE)
+db = client.get_database(mongo_database)
 
 
 def find(collection: str, filters: dict = None, projection: dict = None) -> List[dict]:
     return list(db.get_collection(collection).find(filters, projection))
 
 
-def update_one(collection: str, filters: dict, changes: dict) -> bool:
-    return bool(db.get_collection(collection).update_one(filters, {"$set": changes}))
+def insert_one(collection: str, document: dict) -> dict | None:
+    inserted_id = db.get_collection(collection).insert_one(document).inserted_id
+    if not inserted_id:
+        return None
+    document = copy.deepcopy(document)
+    document["_id"] = inserted_id
+    return document
+
+
+def update_one(
+    collection: str, filters: dict, changes: dict, operation: str = "$set"
+) -> bool:
+    return (
+        db.get_collection(collection)
+        .update_one(filters, {operation: changes})
+        .modified_count
+        == 1
+    )
